b561b07ff3f62b21fdffbebd96cc544513f014fc
Talia Kastrati <kastrati.talia@hotmail.com>
2024-02-26 17:41:46 +0100

    flask_sqlalchemy API

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask, json, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from sqlDatabase import *
 from mongodb import *
-
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\admin\\Desktop\\flaskProject\\database\\users_vouchers.db'
@@ -11,7 +9,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-#many-to-one: user_spending to user_info
+# many-to-one: user_spending to user_info
 class UserInfo(db.Model):
     __tablename__ = 'user_info'
 
@@ -44,23 +42,6 @@ class UserSpending(db.Model):
         }
 
 
-@app.route('/<int:user_id>', methods=['GET'])
-def get_user_test(user_id):
-    user_spending_records = UserSpending.query.filter_by(user_id=user_id).all()
-    print(user_spending_records)
-
-    # Count the number of records for the specific user_id
-    user_id_count = UserSpending.query.filter_by(user_id=user_id).count()
-    print(user_id_count)
-
-    if user_spending_records:
-        result = [record.to_dict() for record in user_spending_records]
-        print(result)
-        return jsonify(result), 200
-    else:
-        return jsonify({"message": "No records found"}), 404
-
-
 # 1. Get all users API
 @app.route('/users', methods=['GET'])
 def get_all_users():
@@ -70,7 +51,6 @@ def get_all_users():
         return jsonify(user_list), 200
     else:
         return jsonify({'error': 'No users'}), 400
-
 
 
 # 2. Get user by id API
@@ -88,34 +68,32 @@ def get_user_by_id(user_id):
 # API 1: TOTAL spending of user_id
 # http://127.0.0.1:5000/total_spent?user_id=35
 @app.route('/total_spent', methods=['GET'])
-def average_spending_by_age():
+def total_spent():
     try:
         user_id = request.args.get('user_id')
 
         if user_id is None:
             return json.dumps({'error': 'Missing user_id parameter'}), 400
 
-        query = """SELECT ui.user_id, ui.name, ui.age, SUM(us.money_spent) as total_spending 
-                FROM user_info ui
-                INNER JOIN user_spending us ON ui.user_id = us.user_id
-                WHERE ui.user_id = ?
-                GROUP BY ui.user_id, ui.name, ui.age
-                """
+        user_info = UserInfo.query.filter_by(user_id=user_id).first()
 
-        result = query_db(query, (user_id,))
-
-        if not result:
+        if user_info is None:
             return json.dumps({'error': 'User not found'}), 404
-        else:
-            user_data = {
-                'user_id': result[0][0],
-                'name': result[0][1],
-                'age': result[0][2],
-                'total_spending': round(result[0][3], 2)
-            }
+
+        total_spending = db.session.query(db.func.sum(UserSpending.money_spent)).filter_by(user_id=user_id).scalar()
+
+        if total_spending is None:
+            total_spending = 0
+
+        user_data = {
+            'user_id': user_info.user_id,
+            'name': user_info.name,
+            'age': user_info.age,
+            'total_spending': round(total_spending, 2)
+        }
 
         print(user_data)
-        return json.dumps(user_data)
+        return jsonify(user_data)
 
     except Exception as e:
         print("Unexpected error:", str(e))
@@ -126,61 +104,86 @@ def average_spending_by_age():
 # http://127.0.0.1:5000/average_spending_by_age/35
 @app.route('/average_spending_by_age/<int:user_id>', methods=['GET'])
 def calculate_average_spending(user_id):
-    user_id = request.view_args['user_id']
+    try:
+        user_info = UserInfo.query.filter_by(user_id=user_id).first()
 
-    query = '''
-            SELECT ui.age, AVG(us.money_spent) as average_spending
-            FROM user_info ui
-            INNER JOIN user_spending us
-            ON ui.user_id = us.user_id
-            WHERE ui.user_id=?
-            GROUP BY ui.age
-    '''
+        if user_info is None:
+            return "User not found", 404
 
-    user_data = query_db(query, (user_id,))
+        age_query = db.session.query(UserInfo.age, db.func.avg(UserSpending.money_spent).label('average_spending')) \
+            .join(UserSpending, UserInfo.user_id == UserSpending.user_id) \
+            .filter(UserInfo.user_id == user_id) \
+            .group_by(UserInfo.age) \
+            .all()
 
-    if user_data:
-        user_age = user_data[0][0]
-        total_spending = round(user_data[0][1], 2)
+        if not age_query:
+            return "User has no spending data", 404
 
-        if 18 <= user_age <= 24:
-            age_group = "18-24"
-        elif 25 <= user_age <= 30:
-            age_group = "25-30"
-        elif 31 <= user_age <= 36:
-            age_group = "31-36"
-        elif 37 <= user_age <= 47:
-            age_group = "37-47"
-        else:
-            age_group = ">47"
+        user_age = age_query[0][0]
+        total_spending = round(age_query[0][1], 2)
+
+        age_group = get_age_group(user_age)
 
         response_data = {
             'user_id': user_id,
             'age': user_age,
             'total_spending': total_spending,
-            "age_group": age_group
+            'age_group': age_group
         }
 
-        return json.dumps(response_data)
-    return "User not found", 404
+        return jsonify(response_data)
+
+    except Exception as e:
+        print("Unexpected error:", str(e))
+        return json.dumps({'error': 'Internal Server Error'}), 500
+
+
+def get_age_group(user_age):
+    if 18 <= user_age <= 24:
+        return "18-24"
+    elif 25 <= user_age <= 30:
+        return "25-30"
+    elif 31 <= user_age <= 36:
+        return "31-36"
+    elif 37 <= user_age <= 47:
+        return "37-47"
+    else:
+        return ">47"
 
 
 # API 3: BONUS calculation
+# curl -X POST -H "Content-Type: application/json" -d "
+# {\"user_id\": 123, \"total_spending\": 2500}" "http://127.0.0.1:5000/write_to_mongodb"
 @app.route('/write_to_mongodb', methods=['POST'])
 def write_to_mongodb():
-    data = request.get_json()
+    try:
+        data = request.get_json()
+        user_id = data.get('user_id')
+        total_spending = data.get('total_spending')
 
-    if collection.find_one({'user_id': data['user_id']}):
-        return json.dumps({'Error': f'User with user_id {data["user_id"]} already exists!'}), 400
-    else:
-        if data['total_spending'] >= 2000:
-            result = collection.insert_one(data)
-            if result.inserted_id:
-                return json.dumps({'Success': 'Data was successfully inserted'}), 201
-            else:
-                return json.dumps({'Error': 'Failed to insert data'}), 500
+        bonus_amount = 2000
+
+        if not user_id or not total_spending:
+            return json.dumps({'error': 'Missing user_id or total_spending parameter'}), 400
+
+        existing_user = collection.find_one({'user_id': user_id})
+
+        if existing_user:
+            return json.dumps({'error': f'User with user_id {user_id} already exists!'}), 400
+
+        if total_spending < bonus_amount:
+            return json.dumps({'error': 'Total spending must be greater than 2000!'}), 400
+
+        result = collection.insert_one(data)
+
+        if result.inserted_id:
+            return json.dumps({'success': 'Data was successfully inserted'}), 201
         else:
-            return json.dumps({'Bad Request': 'Total spending must be greater than 2000 !'}), 400
+            return json.dumps({'error': 'Failed to insert data'}), 500
+
+    except Exception as e:
+        print("Unexpected error:", str(e))
+        return json.dumps({'error': 'Internal Server Error'}), 500
 
 
 if __name__ == '__main__':
